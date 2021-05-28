39f2ca8a5a25d1ffb28f2b3c4622f8d7cd552209
Aleksey Egorov <aleksey358358@gmail.com>
2021-05-28 21:09:04 +0300

    update employee

--- employee_script.sql
+++ employee_script.sql
@@ -1,20 +1,11 @@
 create table if not exists Employee(
     id serial primary key,
-    id_Department integer references Department(id),
-    id_Chief integer references Chief(id),
+    Department varchar(100),
+    Chief integer references Employee(id),
     name varchar(80) not null
 );
 
-create table if not exists Department(
-    id serial primary key,
-    id_Chief integer references Chief(id),
-    name varchar(100) not null
-);
 
-create table if not exists Chief(
-    id serial primary key,
-    id_Department integer references Department(id),
-    name varchar(80) not null
-);
+
 
 
